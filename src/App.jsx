69e3ef0900086927963835e4c09a3a5c943516af
Coder-Towhid <islamsmtowhidul@gmail.com>
2023-10-31 00:22:31 +0600

    project setup & added libraries

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { ThemeProvider } from "styled-components"
+import Footer from "./components/Footer"
+import Navigation from "./components/Navigation"
+import About from "./components/sections/About"
+import Faq from "./components/sections/Faq"
+import Home from "./components/sections/Home"
+import Showcase from "./components/sections/Showcase"
+import Team from "./components/sections/Team"
+import GlobalStyles from "./styles/GlobalStyles"
+import { light } from "./styles/Themes"
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <GlobalStyles/>
+    <ThemeProvider theme={light} >
+    <Navigation/> 
+    <Home/>
+    <About/>
+    <Showcase/>
+    <Team/>
+    <Faq/>
+    <Footer/>
+    </ThemeProvider>
+   
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// import './index.css'
+
+// this will remove default css
+import { BrowserRouter } from "react-router-dom";
+import "../node_modules/normalize.css/normalize.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <App/> 
+      
+    </BrowserRouter>
+  </React.StrictMode>
+);
